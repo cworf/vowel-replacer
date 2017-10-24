cb860dd37b50196078ee0dc74f1c73b77f037dff
Colin Worf and Holly Wilkalis <colin@>
2017-10-24 13:06:40 -0700

    fix code problems, complete

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,32 @@ $(function(){
     event.preventDefault();
     var vowels = ["a","e","i","o","u"];
     var letters = $('#sentence').val().split("");
-    var found = false;
-    var check = letters.map(function(letter){
 
-      for (var index = 0; letter != vowels[index]; index += 1) {
-        return letter;
-        found = true;
+    for (var iLetters = 0; iLetters < letters.length; iLetters += 1) {
+      for (var iVowels = 0; iVowels < vowels.length; iVowels += 1) {
+        if (letters[iLetters] === vowels[iVowels]) {
+          letters[iLetters] = "-";
+        };
       };
-      if (!found) {
-        return "-";
-      };
-    });
-    var newSentence = check.join("");
-    $('.result').text(newSentence);
+    };
+    $('.result').text(letters.join(""));
   });
 });
+
+    // var found = false;
+    // var check = letters.map(function(letter){
+    //
+    //   for (var index = 0; letter === vowels[index]; index += 1) {
+    //     return "-";
+    //     found = true;
+    //   };
+    //   if (!found) {
+    //     return letter;
+    //   };
+    // });
+    // var newSentence = check.join("");
+    // $('.result').text(newSentence);
+
 
 
 // vowels.forEach(function(vowel){
